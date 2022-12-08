32b7e4bd6a18464ce3a0205e6b0c2d8930f222c2
dup0nt <diogo.mfg00@gmail.com>
2022-12-08 15:50:24 +0000

    django + databaseconfig

---
+++ django/craftbeers/brCraftbeers/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class BrcraftbeersConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "brCraftbeers"
--- django/craftbeers/craftbeers/asgi.py
+++ django/craftbeers/craftbeers/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for craftbears project.
+ASGI config for craftbeers project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "craftbears.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "craftbeers.settings")
 
 application = get_asgi_application()
