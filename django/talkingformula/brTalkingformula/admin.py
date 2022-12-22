1a857b26889d30fb4b8e0f33513a45817ca640f5
dup0nt <diogo.mfg00@gmail.com>
2022-12-22 19:59:36 +0000

    admin administration

--- django/talkingformula/brTalkingformula/admin.py
+++ django/talkingformula/brTalkingformula/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Circuito
+from .models import Corrida
+from .models import Piloto
+from .models import PilotoEquipa
+from .models import Equipa
+from .models import Construtor
+
+admin.site.register(Circuito)
+admin.site.register(Corrida)
+admin.site.register(Piloto)
+admin.site.register(PilotoEquipa)
+admin.site.register(Equipa)
+admin.site.register(Construtor)
--- django/talkingformula/talkingformula/settings.py
+++ django/talkingformula/talkingformula/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
-import pymysql
+#import pymysql
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -53,7 +53,7 @@ DATABASES = {
     }
 }
 
-pymysql.install_as_MySQLdb()
+#pymysql.install_as_MySQLdb()
 #python manage.py inspectdb > brTalkingformula/models.py     
 
 MIDDLEWARE = [
@@ -133,7 +133,7 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [ os.path.join(BASE_DIR,'static') ]
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
